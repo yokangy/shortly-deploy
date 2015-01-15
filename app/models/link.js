23a2a252bdccbda019752b5e2c4b9a2258db4923
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 12:00:17 -0800

    Modified saveLink to save the link code synchronously.

--- app/models/link.js
+++ app/models/link.js
@@ -20,8 +20,9 @@ var mongoose = require('mongoose');
 db.urlsSchema.methods.createCode = function(){
   var shasum = crypto.createHash('sha1');
   shasum.update(this.url);
-  this.code = shasum.digest('hex').slice(0, 5);
-  this.save();
+  return shasum.digest('hex').slice(0, 5);
+  // this.code = shasum.digest('hex').slice(0, 5);
+  // this.save();
 };
 
 var Link = mongoose.model('Link', db.urlsSchema);
