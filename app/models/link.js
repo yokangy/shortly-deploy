b9bc748bb06693b36dbb6ffabd19d69b0c529598
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 19:47:42 -0800

    Refactored navToLink to use findOne instead of find.

--- app/models/link.js
+++ app/models/link.js
@@ -2,21 +2,6 @@ var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-//
 db.urlsSchema.pre('save', function(next){
   var shasum = crypto.createHash('sha1');
   shasum.update(this.url);
