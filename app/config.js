2f5b5fe096a47b3273a0e301809e06e77a2b7e45
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 16:05:52 -0800

    Password hashing is now triggered when saving a new user.

--- app/config.js
+++ app/config.js
@@ -1,61 +1,19 @@
 // var Bookshelf = require('bookshelf');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// var path = require('path');
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
 
 var connectionString = process.env.APPSETTING_DB_CONNECTION_STRING || 'mongodb://localhost/shortlydb';
 mongoose.connect(connectionString);
 
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-  module.exports.urlsSchema = new Schema({
-    url: String,
-    base_url: String,
-    code: String,
-    title: String,
-    visits: Number
-  });
-    // db.knex.schema.createTable('urls', function (link) {
-    //   link.increments('id').primary();
-    //   link.string('url', 255);
-    //   link.string('base_url', 255);
-    //   link.string('code', 100);
-    //   link.string('title', 255);
-    //   link.integer('visits');
-    //   link.timestamps();
-    // }).then(function (table) {
-    //   console.log('Created Table', table);
-    // });
-  // }
-// });
+module.exports.urlsSchema = new Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number
+});
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-  module.exports.usersSchema = new Schema({
-    username: String,
-    password: String
-  });
-
-// module.exports = db;
+module.exports.usersSchema = new Schema({
+  username: String,
+  password: String
+});
--- app/models/user.js
+++ app/models/user.js
@@ -3,41 +3,20 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
 
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
-//
-
 db.usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
   bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
     callback(isMatch);
   });
 };
 
-db.usersSchema.methods.hashPassword = function(){
+db.usersSchema.pre('save', function(next){
   var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
+  cipher(this.password, null, null).bind(this)
     .then(function(hash) {
       this.password = hash;
-      this.save();
+      next();
     });
-};
+});
 
 var User = mongoose.model('User', db.usersSchema);
 
